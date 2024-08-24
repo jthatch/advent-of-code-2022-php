399f383c5ed53924f2792fc7c7852dd31cb24eac
James Thatcher <james@jameswt.com>
2024-08-24 01:35:08 +0100

    complete day 3

---
+++ src/Days/Day3.php
@@ -0,0 +1,78 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Days;
+
+use App\Contracts\Day;
+use Illuminate\Support\Collection;
+
+class Day3 extends Day
+{
+    public const EXAMPLE1 = <<<eof
+        vJrwpWtwJgWrhcsFMMfFFhFp
+        jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+        PmmdzqPrVvPwwTWBwg
+        wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+        ttgJtRGJQctTZtZT
+        CrZsJsPPZsGzwwsLwLmpwMDw
+        eof;
+
+    /**
+     * Find the item type that appears in both compartments of each rucksack. What is the sum of the priorities of those item types?
+     */
+    public function solvePart1(mixed $input): int|string|null
+    {
+        //  given rucksack always has the same number of items in each of its two compartments,
+        // so the first half of the characters represent items in the first compartment,
+        // while the second half of the characters represent items in the second compartment.
+        return $this->parseInput($input)
+            // split the characters in half for each compartment in the rucksack
+            ->map(function (string $line) {
+                $halfLength = intdiv(strlen($line), 2);
+
+                return [
+                    substr($line, 0, $halfLength),
+                    substr($line, $halfLength),
+                ];
+            })
+            // find common characters
+            ->map(function (array $parts) {
+                [$part1, $part2] = $parts;
+                $commonChars     = array_intersect(
+                    str_split($part1),
+                    str_split($part2)
+                );
+
+                return array_unique($commonChars);
+            })
+            // convert to a priority
+            ->map(
+                fn (array $commonChars) => collect($commonChars)
+                    ->map(
+                        fn ($char) => ctype_lower($char)
+                            ? ord($char) - ord('a') + 1 // a-z: 1-26
+                            : ord($char) - ord('A') + 27 // A-Z: 27-52
+                    )->toArray()
+            )
+            // sum them all
+            ->map(fn ($chunk) => array_sum($chunk))->sum();
+    }
+
+    public function solvePart2(mixed $input): int|string|null
+    {
+        return '';
+    }
+
+    protected function parseInput(mixed $input): Collection
+    {
+        $input = is_array($input) ? $input : explode("\n", $input);
+
+        return collect($input);
+    }
+
+    public function getExample2(): mixed
+    {
+        return static::EXAMPLE1;
+    }
+}
