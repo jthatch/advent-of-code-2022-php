f158253db47d8f40a45f0ee13cb1e69841b84988
James Thatcher <james@jameswt.com>
2024-08-31 00:05:37 +0100

    completed day 8 part 1

--- src/Days/Day8.php
+++ src/Days/Day8.php
@@ -17,76 +17,97 @@ class Day8 extends Day
         35390
         eof;
 
+    // [[y,x],..] each of the 4 possible adjacent locations, starting from top going clockwise
+    private array $adjacent = [[-1, 0], [0, 1], [1, 0], [0, -1]];
+
     public function solvePart1(mixed $input): int|string|null
     {
         $input = $this
             ->parseInput($input)
             ->toArray();
-        $adjacent = [[-1, 0], [0, 1], [1, 0], [0, -1]];
 
         $visible = [];
 
+        // loop over our input in y,x coordinates
         for ($y = 0, $yMax = count($input); $y < $yMax; ++$y) {
             for ($x = 0, $xMax = count($input[0]); $x < $xMax; ++$x) {
-                $location = $input[$y][$x];
-                $key      = sprintf('%s-%s', $y, $x);
-                printf("key: %s location: %s\n", $key, $location);
+                $isEdge = 0 === $y || 0 === $x || $y === ($yMax - 1) || $x === ($xMax - 1);
+                $key    = sprintf('%s-%s', $y, $x);
+                $height = $input[$y][$x];
 
-                // A tree is visible if all of the other trees between it and an edge of the grid are shorter than it.
-                // Only consider trees in the same row or column; that is, only look up, down, left, or right from any given tree.
-                if (!empty(array_filter(
-                    $adjacent,
-                    function (array $pos) use ($y, $x, $input, $location): bool {
-                        $newY        = $y + $pos[0];
-                        $newX        = $x + $pos[1];
-                        $locAdjacent = $input[$y + $pos[0]][$x + $pos[1]] ?? null;
-                        if (null === $locAdjacent) {
+                // if our tree is on the edge the rules are it's always visible so handle this now
+                if ($isEdge) {
+                    $visible[$key] = $height;
+                    continue;
+                }
+
+                // otherwise, from our starting location traverse in each direction until we hit a the edge
+                // a tree is visible if it's the tallest in at least one direction
+                $isVisible = false;
+                collect($this->adjacent)
+                    ->each(function (array $pos) use ($y, $x, $height, $input, &$isVisible) {
+                        // tree is already deemed visible so we can break out of further adjacent loops
+                        if ($isVisible) {
                             return true;
                         }
-                        // todo solve this
-                        printf("y: %s x: %s ay: %s ax: %s\n", $y, $x, $y + $pos[0], $x + $pos[1]);
-                        // 22 but needs to be 21
-                        /**
-                         * 2^ array:22 [
-                         * "0-0" => "3"
-                         * "0-1" => "0"
-                         * "0-2" => "3"
-                         * "0-3" => "7"
-                         * "0-4" => "3"
-                         * "1-0" => "2"
-                         *
-                         * "1-1" => "5"
-                         * "1-2" => "5"
 
-                         * "1-4" => "2"
-                         *
-                         * "2-0" => "6"
-                         *
-                         * "2-1" => "5"
-                         * "2-3" => "3"
-                         * "2-4" => "2"
-                         * "3-0" => "3"
-                         * "3-2" => "5"
-                         * "3-3" => "4"
-                         * "3-4" => "9"
-                         * "4-0" => "3"
-                         * "4-1" => "5"
-                         * "4-2" => "3"
-                         * "4-3" => "9"
-                         * "4-4" => "0"
-                         * ]
- */
-                        return $location > $locAdjacent;
-                    }
-                ))) {
-                    $key           = sprintf('%s-%s', $y, $x);
-                    $visible[$key] = $location;
+                        while (true) {
+                            // keep travelling in the adjacent direction
+                            $y += $pos[0];
+                            $x += $pos[1];
+                            $nextHeight = $input[$y][$x] ?? null;
+
+                            if (null === $nextHeight) {
+                                break;
+                            }
+
+                            if ($height <= $nextHeight) {
+                                $isVisible = false;
+                                break;
+                            }
+
+                            $isVisible = true;
+                        }
+                    });
+
+                if ($isVisible) {
+                    $visible[$key] = $height;
                 }
             }
         }
-        dd('visible', $visible);
 
-        return ''; // should be 21
+        return count($visible);
+    }
+
+    /**
+     * Working with coordinates isn't really collect()'s strong suit, so did this the old-fashioned way.
+     *
+     * @return array<string, int> $input key in format `y-x`
+     */
+    protected function getLowPoints(array $heightmap): array
+    {
+        $lowPoints = [];
+        for ($y = 0, $yMax = count($heightmap); $y < $yMax; ++$y) {
+            for ($x = 0, $xMax = count($heightmap[0]); $x < $xMax; ++$x) {
+                $height = $heightmap[$y][$x];
+
+                // loop over our adjacent positions, if none are bigger then we've found a low point
+                if (empty(array_filter(
+                    $this->adjacent,
+                    function (array $pos) use ($height, $heightmap, $y, $x, $yMax, $xMax): bool {
+                        $adjacent = $heightmap[$y + $pos[0]][$x + $pos[1]] ?? -1;
+                        $isEdge   = 0 === $y || 0 === $x || $y === ($yMax - 1) || $x === ($xMax - 1);
+
+                        return $isEdge || $height > $adjacent;
+                    }
+                ))) {
+                    $key             = sprintf('%s-%s', $y, $x);
+                    $lowPoints[$key] = $height;
+                }
+            }
+        }
+
+        return $lowPoints;
     }
 
     public function solvePart2(mixed $input): int|string|null
