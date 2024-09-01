bef7637bf6b6e4d8b52580161567e5ebbc2bb536
James Thatcher <james@jameswt.com>
2024-09-01 21:17:03 +0100

    completed day 8 part 2

--- src/Days/Day8.php
+++ src/Days/Day8.php
@@ -20,6 +20,9 @@ class Day8 extends Day
     // [[y,x],..] each of the 4 possible adjacent locations, starting from top going clockwise
     private array $adjacent = [[-1, 0], [0, 1], [1, 0], [0, -1]];
 
+    /**
+     * Consider your map; how many trees are visible from outside the grid?
+     */
     public function solvePart1(mixed $input): int|string|null
     {
         $input = $this
@@ -28,93 +31,114 @@ class Day8 extends Day
 
         $visible = [];
 
-        // loop over our input in y,x coordinates
-        for ($y = 0, $yMax = count($input); $y < $yMax; ++$y) {
-            for ($x = 0, $xMax = count($input[0]); $x < $xMax; ++$x) {
-                $isEdge = 0 === $y || 0 === $x || $y === ($yMax - 1) || $x === ($xMax - 1);
-                $key    = sprintf('%s-%s', $y, $x);
-                $height = $input[$y][$x];
+        foreach ($this->treeIterator($input) as [$key, $y, $x, $yMax, $xMax, $height, $isEdge]) {
+            // if our tree is on the edge the rules are it's always visible so handle this now
+            if ($isEdge) {
+                $visible[$key] = $height;
+                continue;
+            }
 
-                // if our tree is on the edge the rules are it's always visible so handle this now
-                if ($isEdge) {
-                    $visible[$key] = $height;
-                    continue;
-                }
+            // otherwise, from our starting location traverse in each direction until we hit a the edge
+            // a tree is visible if it's the tallest in at least one direction
+            $isVisible = false;
+            collect($this->adjacent)
+                ->each(function (array $pos) use ($y, $x, $height, $input, &$isVisible) {
+                    // tree is already deemed visible so we can break out of further adjacent loops
+                    if ($isVisible) {
+                        return true;
+                    }
 
-                // otherwise, from our starting location traverse in each direction until we hit a the edge
-                // a tree is visible if it's the tallest in at least one direction
-                $isVisible = false;
-                collect($this->adjacent)
-                    ->each(function (array $pos) use ($y, $x, $height, $input, &$isVisible) {
-                        // tree is already deemed visible so we can break out of further adjacent loops
-                        if ($isVisible) {
-                            return true;
+                    while (true) {
+                        // keep travelling in the adjacent direction
+                        $y += $pos[0];
+                        $x += $pos[1];
+                        // applying null if we exceed the boundaries (go past the edge)
+                        $nextHeight = $input[$y][$x] ?? null;
+
+                        // break when we hit the edge
+                        if (null === $nextHeight) {
+                            break;
                         }
 
-                        while (true) {
-                            // keep travelling in the adjacent direction
-                            $y += $pos[0];
-                            $x += $pos[1];
-                            $nextHeight = $input[$y][$x] ?? null;
-
-                            if (null === $nextHeight) {
-                                break;
-                            }
-
-                            if ($height <= $nextHeight) {
-                                $isVisible = false;
-                                break;
-                            }
-
-                            $isVisible = true;
+                        // we've hit a tree taller than ourselves, therefore we aren't visible
+                        if ($height <= $nextHeight) {
+                            $isVisible = false;
+                            break;
                         }
-                    });
 
-                if ($isVisible) {
-                    $visible[$key] = $height;
-                }
+                        // otherwise we are still the tallest tree
+                        $isVisible = true;
+                    }
+                });
+
+            // ultimately if we are visible record it
+            if ($isVisible) {
+                $visible[$key] = $height;
             }
         }
 
         return count($visible);
     }
 
+    public function solvePart2(mixed $input): int|string|null
+    {
+        $input = $this
+            ->parseInput($input)
+            ->toArray();
+
+        $scenicScores = [];
+
+        foreach ($this->treeIterator($input) as [$key, $y, $x, $yMax, $xMax, $height, $isEdge]) {
+            // the number of trees we can see over
+            $visibility = [];
+            collect($this->adjacent)
+                ->each(function (array $pos) use ($y, $x, $height, $input, &$visibility) {
+                    $visible = 0;
+                    while (true) {
+                        // keep travelling in the adjacent direction
+                        $y += $pos[0];
+                        $x += $pos[1];
+                        $nextHeight = $input[$y][$x] ?? null;
+
+                        // break when we hit the edge
+                        if (null === $nextHeight) {
+                            break;
+                        }
+
+                        ++$visible;
+
+                        // we've hit a tree taller than ourselves, therefore we aren't visible
+                        if ($height <= $nextHeight) {
+                            break;
+                        }
+                    }
+                    $visibility[] = $visible;
+                });
+            // multiply the values together
+            $scenicScores[$key] = array_reduce($visibility, fn ($carry, $item) => $carry * $item, 1);
+        }
+
+        return collect($scenicScores)->max();
+    }
+
     /**
      * Working with coordinates isn't really collect()'s strong suit, so did this the old-fashioned way.
      *
-     * @return array<string, int> $input key in format `y-x`
+     * @param array $input array<string, int> $input key in format `y-x`
+     *
+     * @return \Generator array<string, int, int, int, int, int, bool>
      */
-    protected function getLowPoints(array $heightmap): array
+    protected function treeIterator(array $input): \Generator
     {
-        $lowPoints = [];
-        for ($y = 0, $yMax = count($heightmap); $y < $yMax; ++$y) {
-            for ($x = 0, $xMax = count($heightmap[0]); $x < $xMax; ++$x) {
-                $height = $heightmap[$y][$x];
-
-                // loop over our adjacent positions, if none are bigger then we've found a low point
-                if (empty(array_filter(
-                    $this->adjacent,
-                    function (array $pos) use ($height, $heightmap, $y, $x, $yMax, $xMax): bool {
-                        $adjacent = $heightmap[$y + $pos[0]][$x + $pos[1]] ?? -1;
-                        $isEdge   = 0 === $y || 0 === $x || $y === ($yMax - 1) || $x === ($xMax - 1);
-
-                        return $isEdge || $height > $adjacent;
-                    }
-                ))) {
-                    $key             = sprintf('%s-%s', $y, $x);
-                    $lowPoints[$key] = $height;
-                }
+        // loop over our input in y,x coordinates
+        for ($y = 0, $yMax = count($input); $y < $yMax; ++$y) {
+            for ($x = 0, $xMax = count($input[0]); $x < $xMax; ++$x) {
+                $isEdge = 0 === $y || 0 === $x || $y === ($yMax - 1) || $x === ($xMax - 1);
+                $key    = sprintf('%s-%s', $y, $x);
+                $height = $input[$y][$x];
+                yield [$key, $y, $x, $yMax, $xMax, $height, $isEdge];
             }
         }
-
-        return $lowPoints;
-    }
-
-    public function solvePart2(mixed $input): int|string|null
-    {
-        $input = $this->parseInput($input);
-
-        return '';
     }
 
     protected function parseInput(mixed $input): Collection
